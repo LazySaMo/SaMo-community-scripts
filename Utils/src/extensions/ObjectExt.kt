f2a7e57f0d6db8168dd2f1c8a4c5a6fb4ef602a7
LazySaMo <abdsaam17@gmail.com>
2025-08-29 00:20:34 +0100

    Update jars

--- Utils/src/extensions/ObjectExt.kt
+++ Utils/src/extensions/ObjectExt.kt
@@ -1,8 +1,31 @@
 package extensions
 
+import com.osmb.api.location.position.types.WorldPosition
 import com.osmb.api.scene.RSObject
+import com.osmb.api.script.Script
+import com.osmb.api.shape.Polygon
 
 fun RSObject.interactRawText(rawTextEquals: String): Boolean =
     interact { entries ->
-        entries.firstOrNull { it.rawText.equals(rawTextEquals, ignoreCase = true) }
+        entries.firstOrNull { it.rawText.contains(rawTextEquals, ignoreCase = true) }
     }
+
+fun Script.tapProjected(
+    pos: WorldPosition,
+    actionText: String? = null,
+    cubeHeight: Int = 160,
+    scale: Double = 0.7,
+    requireOnScreen: Boolean = false
+): Boolean {
+    val poly: Polygon = sceneProjector
+        .getTileCube(pos, cubeHeight, requireOnScreen)
+        ?.getResized(scale) ?: return false
+
+    return if (actionText != null) {
+        finger.tap(true, poly, { entries ->
+            entries.firstOrNull { it.rawText.contains(actionText, ignoreCase = true) }
+        })
+    } else {
+        finger.tap(true, poly)
+    }
+}
--- Utils/src/timers/RandomTimer.kt
+++ Utils/src/timers/RandomTimer.kt
@@ -5,3 +5,5 @@ import com.osmb.api.utils.RandomUtils
 fun randomIdleTimer(): Long = RandomUtils.uniformRandom(8000, 12000).toLong()
 
 fun randomPlayerAnimatingTimer(): Long = RandomUtils.uniformRandom(1500, 2000).toLong()
+
+fun randomMixologyPlayerAnimatingTimer(): Long = RandomUtils.uniformRandom(1100, 1300).toLong()
